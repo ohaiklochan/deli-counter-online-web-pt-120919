599a54a1442782f53da58b96c69ce32f3a731a78
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:54:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 
 def line(katz_deli)
-  if katz_deli.empty
+  if katz_deli.empty?
     puts "The line is currently empty."
-  else katz_deli.empty? == false
+  else 
     position = 1
     katz_deli.each do |person|
       places << "#{position}. #{person}."
