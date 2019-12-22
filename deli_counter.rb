9bd3d1c6c21a7283e248696d9edb7b69a254479d
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:50:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else katz_deli.empty? == false
     position = 1
-    katz_deli.each do |position|
+    katz_deli.each do |person|
       
 end
 
