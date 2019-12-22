ae9de5bc743c9a14062fd819f2bd9cdc5cbb3554
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:58:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,8 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else 
-    number = 1
-    katz_deli.each.with_index do |person, number|
+    katz_deli.each.with_index(1) do |person, number|
       position << "#{number}. #{person}."
-      number += 1
     end
     puts "The line is currently: "
 end
