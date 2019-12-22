8021bf2ac82ccb5233d3c8a01768546f20283674
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:39:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line(array)
   if line.size == 0
     puts "The line is currently empty."
   elsif line.size > 0
+    array.each_with_index do |value, 
     puts "The line is currently: #{"
 end
 
