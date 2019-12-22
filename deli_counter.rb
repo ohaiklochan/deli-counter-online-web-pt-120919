87532c5025ee6e926671eddffb1c7bb7af229d23
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:53:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,3 @@ def line(katz_deli)
     puts "The line is currently: #{places.join(" ")}."
 end
 
-def take_a_number(array, name)
-  array.push (name)
-  place = array.index (name)
-  puts "Welcome #{name}! You are number #{array.index(name+1)} in line!"
-  return name, place
-end
