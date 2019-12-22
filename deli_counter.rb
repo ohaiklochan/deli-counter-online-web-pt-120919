8a134998554676ea386b6329478070ad8bc96bb0
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 21:11:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(array, name)
   array.push (name)
   place = array.index (name)
-  puts "Welcome, #{name}! You are number #{array.index(name+1)} in line!"
+  puts "Welcome, #{name}! You are number #{array.index(1)} in line!"
   puts name, place
 end
 
